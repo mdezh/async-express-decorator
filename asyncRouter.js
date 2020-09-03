5e0570a8a6c75e9489986346b666398a5a690b32
GusGold <me@angusgoldman.com.au>
2020-09-04 01:25:23 +1000

    Updates to asyncHandle all route layers
    not just those defined via router.[method]()

--- asyncRouter.js
+++ asyncRouter.js
@@ -1,5 +1,7 @@
 const asyncHandler = (func) => (req, res, next) => Promise.resolve(func(req, res, next)).catch(next)
 
+// Default methods to use when calling toAsyncRouter
+// must be lower case
 let methods = [
     'get',
     'post',
@@ -8,17 +10,61 @@ let methods = [
 ]
 
 function toAsyncRouter(router) {
-    for (let key in router) {
-        if (methods.includes(key)) {
-            let method = router[key]
-            router[key] = (path, ...callbacks) => method.call(router, path, ...callbacks.map(cb => asyncHandler(cb)))
+    // Store reference to the original function for calling later with our extended `route` object
+	const thisRoute = router.route
+
+    // Intercept the route function. This is implicitily called when using `router.[method]()` and explicitly called when using `router.route().[method]()`
+    router.route = (path) => {
+        // Store reference to the object returned from creating the new route
+        const route = thisRoute.call(router, path)
+
+        // Create a new data store like `route.methods` so that ...
+        route.__methods = {}
+
+        // ... for each method to be wrapped, we can ...
+        for (const method of methods) {
+            Object.defineProperty(route.methods, method, {
+                // ... create a setter for the method ...
+                set: (val) => {
+                    if (val === true) {
+                        // ... to intercept a new express layer being created ...
+                        const thisLayerIndex = route.stack.length
+                        setImmediate(() => {
+                            const layer = route.stack[thisLayerIndex]
+                            // ... and wrap the `handle` in the `asyncHandler`
+                            layer.handle = asyncHandler(layer.handle)
+                        })
+                    }
+
+                    Object.defineProperty(route.methods, method, {
+                        // Make `method` visible on `route.methods` once value has been set for `Object.keys()` etc.
+                        enumerable: true
+                    })
+
+                    // set the val to the new data store
+                    route.__methods[method] = val
+                },
+                // retrieve value from the new data store
+                get: () => {
+                    return route.__methods[method]
+                },
+                // Allows us to update the `enumerable` property in the setter
+                configurable: true
+            })
         }
+        // Note: for methods not defined in the `toAsyncRouter` `methods` array, `route.methods` is still an object which can have properties set and read as per normal
+
+        // return back our extended route object for chaining with methods
+        return route
     }
+    
+    // return back our extended router
     return router
 }
 
 toAsyncRouter.setMethods = (methodsArray) => {
-    methods = methodsArray.slice()
+    // ensure each method is lower case
+    methods = methodsArray.map(method => String(method).toLowerCase())
 }
 
 toAsyncRouter.getMethods = () => methods.slice()
